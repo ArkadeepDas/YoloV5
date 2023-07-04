fba3736f6012b058700e4fd13e202d80ca99288d
Arkadeep Das <arkadeep.das8@gmail.com>
2023-07-04 18:10:13 +0530

    Example of YoloV5

--- Yolo_V5_Example.py
+++ Yolo_V5_Example.py
@@ -1,5 +1,17 @@
 import torch
-
+import cv2
 # Importing pretrain Yolov5 model
 # Yolov5 have many models. But here we are using small version s
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', device=0)
+
+# capturing image from link
+image = 'https://www.livemint.com/rf/Image-621x414/LiveMint/Period1/2012/10/01/Photos/Road621.jpg'
+
+# Check prediction
+results = model(image)
+# Print output
+print(results)
+# Print co-ordinates
+print(results.pandas().xyxy[0])
+# Show the output image
+results.show()
