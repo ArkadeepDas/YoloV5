7ab0474e8db9950210e82f78d62a2e0814383a2f
Arkadeep Das <arkadeep.das8@gmail.com>
2023-07-06 19:10:52 +0530

    Face and smile detection

---
+++ Object_Detection_Using_MobileCam.py
@@ -0,0 +1,52 @@
+import cv2
+
+# This CascadeClassifier is for object detection
+# Here we are using front face detection algorithm
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                     'haarcascade_frontalface_default.xml')
+# Here we are using smile detection
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                      'haarcascade_smile.xml')
+
+# VideoCapture funtion to record video from mobile phone
+video = cv2.VideoCapture(0)
+address = 'http://192.168.0.104:8080/video'
+# Let's open the video
+video.open(address)
+
+while True:
+    # This function returns two variables
+    # check = boolean variable, indicates whether the frame successfully read or not
+    # frame = actual frame that was read from video source
+    check, frame = video.read()
+    if not check:
+        print("No frame read from video")
+        break
+    # First parameter is input image
+    # Second parameter is how zoomed my image is, we must have to set a value where the output don't capture any noise.
+    # If we set it to 1 or close to 1 then it can capture noise and it takes longer time to process
+    # Third parameter is minimum neighbour pixels to calculate the face
+    face = face_cascade.detectMultiScale(frame,
+                                         scaleFactor=1.5,
+                                         minNeighbors=5)
+    # Output = [[x, y, h, w]]
+
+    # Inside the face we have to calculate the smile
+    for x, y, w, h in face:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        crop_face = frame[y:y + h, x:x + w]
+        # Inside face we are trying to detect smile
+        smile = smile_cascade.detectMultiScale(crop_face,
+                                               scaleFactor=1.5,
+                                               minNeighbors=20)
+        for x, y, w, h in smile:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    cv2.imshow('Output', frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows
