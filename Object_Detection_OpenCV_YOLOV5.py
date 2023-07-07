e5bd287208881d35b4f305e21e8b3909133b459a
Arkadeep Das <arkadeep.das8@gmail.com>
2023-07-07 13:24:33 +0530

    Object detection using mobile camera(Opencv, YoloV5)

--- Object_Detection_OpenCV_YOLOV5.py
+++ Object_Detection_OpenCV_YOLOV5.py
@@ -5,6 +5,9 @@ import numpy as np
 import cv2
 from time import time
 
+# URL for video
+URL = 'http://192.168.0.104:8080/video'
+
 
 # Let's create Object detection class
 class ObjectDetection:
@@ -12,14 +15,13 @@ class ObjectDetection:
     Class implements YoloV5 model to make prediction on video using OpenCV
     '''
 
-    def __init__(self, url, out_file):
+    def __init__(self, url):
         '''
         Initialize the class with url and output file.
         : parameter url: Has to be as video URL, on which the prediction will happen.
         : parameter out_file: Our output file name, where we save our output.
         '''
         self.url = url
-        self.out_file = out_file
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print('\nDevice Used here is: ', self.device)
 
@@ -28,12 +30,11 @@ class ObjectDetection:
         '''
         Load video from link
         '''
-        video = cv2.VideoCapture(0)
+        video = cv2.VideoCapture()
         video.open(self.url)
         check, frame = video.read()
-        if not check:
-            return 0
-        return frame
+
+        return check, frame
 
     # Load model from pytorch
     def load_model(self):
@@ -46,15 +47,14 @@ class ObjectDetection:
         return model
 
     # Takes single frame as a input and gives the output from YoloV5
-    def score_frame(self, frame):
+    def score_frame(self, frame, model):
         '''
         Take single frame as a input and predict is using YoloV5
         : parameter frame: single frame from out video
         : return: lebels and coordinates of objects detected by YoloV5
         '''
-        self.model.to(self.device)
+        model.to(self.device)
         frame = [frame]
-        model = self.load_model()
         results = model(frame)
 
         # Labels and Coordinates
@@ -62,7 +62,7 @@ class ObjectDetection:
         return output
 
     # Let's plot the box
-    def plot_boxes(self, results, frame):
+    def plot_boxes(self, results, frame, model):
         '''
         Take a frame and its results as input and plot the bounding boxs and labels on the frame
         '''
@@ -76,7 +76,34 @@ class ObjectDetection:
                     data[1] * y_shape), int(data[2] * x_shape), int(data[3] *
                                                                     y_shape)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cv2.putText(frame, data[6], (x2, y2), cv2.FONT_HERSHEY_SIMPLEX,
-                            (0, 0, 255))
+                cv2.putText(frame, model.names[int(data[5])], (x2, y2),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
 
         return frame
+
+
+# Let's create the object
+object_detection = ObjectDetection(URL)
+
+# Let's initialize the model
+model = object_detection.load_model()
+
+while True:
+    check, frame = object_detection.get_video_from_url()
+
+    # If there is no frame then break
+    if not check:
+        print("No frame read from video")
+        break
+    output = object_detection.score_frame(frame=frame, model=model)
+    output_frame = object_detection.plot_boxes(results=output,
+                                               frame=frame,
+                                               model=model)
+    # Let's show the
+    cv2.imshow('Output', output_frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows
